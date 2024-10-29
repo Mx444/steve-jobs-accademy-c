b6b5515fcdf64809e788f8a92b364af2519d4e5b
Mx444 <mybbtester@gmail.com>
2024-10-29 11:43:56 +0100

    29/10 costrutto for

--- src/exercises/costrutto-for.c
+++ src/exercises/costrutto-for.c
@@ -5,6 +5,11 @@ int main()
     int i;
     for (i = 0; i <= 10; i++)
     {
-        printf("%d\n", i);
+        printf("Il numero e': %d\n", i);
+        if (i == 5)
+        {
+            printf("Il numero e' 5\n");
+            break;
+        }
     }
 }
